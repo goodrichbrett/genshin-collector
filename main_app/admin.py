e369dc4085178f0a7a8eb0cf23381118fe6f03ba
goodrichbrett <brettgoodrich@outlook.com>
2020-10-07 13:30:23 -0500

    add leveling model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Character
+from .models import Character, Leveling
 # Register your models here.
 admin.site.register(Character)
+admin.site.register(Leveling)
--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.urls import reverse
+
+EXPERIENCE = (
+    ('HP', 'Hitpoints'),
+    ('DE', 'Defense'),
+    ('AT', 'Attack')
+)
+
+
 # Create your models here.
-
-
 class Character(models.Model):
     name = models.CharField(max_length=100)
     gender = models.CharField(max_length=100)
@@ -14,3 +20,17 @@ class Character(models.Model):
 
     def get_absolute_url(self):
         return reverse("detail", kwargs={"character_id": self.id})
+
+
+class Leveling(models.Model):
+    date = models.DateField('leveled Up Date')
+    exp = models.CharField(
+        max_length=2,
+        choices=EXPERIENCE,
+        default=EXPERIENCE[0][0]
+    )
+
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.get_exp_display()} on {self.date}'
